efd654b9a61c676ed38a02bfb6039271ab6d1090
Miyne <mine.gaft@gmail.com>
2021-09-08 20:34:36 +0200

    removing no longer relevant part

--- script/index.js
+++ script/index.js
@@ -23,23 +23,6 @@ function copyText(element) {
 }
 
 document.addEventListener("DOMContentLoaded",function(e) {
-    function getMonthName(nb){
-        switch(nb){
-            case 0:return "Janvier";
-            case 1:return "Février";
-            case 2:return "Mars";
-            case 3:return "Avril";
-            case 4:return "Mai";
-            case 5:return "Juin";
-            case 6:return "Juillet";
-            case 7:return "Août";
-            case 8:return "Septembre";
-            case 9:return "Octobre";
-            case 10:return "Novembre";
-            case 11:return "Décembre";
-        }
-    }
-
     var yellowPin = L.icon({
         iconUrl: 'images/yellowPin.png',
         iconSize:     [70, 70], // size of the icon
@@ -65,24 +48,12 @@ document.addEventListener("DOMContentLoaded",function(e) {
     });
 
     mymap.addEventListener('mousemove', function(e){
-        if(mymap.getCenter().equals([latitudeSlhs, longitudeSlhs], 0.001)){
+        if(mymap.getCenter().equals([latitudeSlhs, longitudeSlhs], 0.0001)){
             document.getElementById("slhs").classList.add("est_centre");
         }else{
             document.getElementById("slhs").classList.remove("est_centre");
         }
     });
-
-/*
-    let jourActuel = new Date();
-    let collecte = new Date();
-    if(jourActuel.getDay()>=collecte.getDay() && jourActuel.getHours()>=17){
-        collecte.setDate(collecte.getDate() + (7-collecte.getDay())%7+1);
-    }else{
-        collecte.setDate(collecte.getDate() + ((7-collecte.getDay())%7+1) % 7);
-    }
-    let mois=getMonthName(collecte.getMonth());
-    document.getElementById("collecte").innerHTML+=" Lundi "+collecte.getDate()+" "+mois+" de 15h à 17h.";
-*/
 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
